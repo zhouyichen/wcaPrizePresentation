8f7e78634dddc3f070a725585ab237b783f56fd1
zhouyichen <zhouyichen123@gmail.com>
2023-01-24 16:32:45 +0800

    fix dnf results

--- prizeJs/prize.js
+++ prizeJs/prize.js
@@ -1,6 +1,9 @@
 const compId = 'Euro2022';
 const compName = "Singapore Championship 2023"
-const countryIso2 = "MY";
+// const countryIso2 = "MY";
+const targetCountryIso2 = "DK";
+const specialCategoryName = "Singaporean Category";
+const openCategoryName = "Open Category";
 
 const defaultEventSqeunce = [
     '555bf', '444bf', '333mbf',
@@ -13,6 +16,7 @@ const defaultEventSqeunce = [
 
 wcifLink =  "https://www.worldcubeassociation.org/api/v0/competitions/" + compId + "/wcif/public"; 
 wcifLink = 'mcc2022.json';
+wcifLink = 'euro2022.json';
 
 var eventIdToRounds = new Map();
 let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
@@ -29,47 +33,106 @@ $.getJSON(wcifLink, function(data) {
     });
     var slides = [];
 
-    idToPerson = {};
+    var idToPerson = {};
+    var targetIds = [];
     data.persons.forEach(person => {
         idToPerson[person.registrantId] = person;
+        if (person.countryIso2 === targetCountryIso2) {
+            targetIds.push(person.registrantId);
+        }
     });
 
     defaultEventSqeunce.forEach(eventId => {
         var eventRounds = eventIdToRounds[eventId];
-        var slide = {};
+        
         if (eventRounds) {
             num_rounds = eventRounds.length;
             const lastRound = eventRounds[num_rounds-1];
+            const format = formats[lastRound.format];
+            var slide = {};
+            var filteredRes = [];
+            var includedTopPlayers = [];
+            for (var r in lastRound.results) {
+                const res = lastRound.results[r];
+                if (targetIds.includes(res.personId)) {
+                    const resTime = res[format.res];
+                    if (resTime < 0) {
+                        continue;
+                    }
+                    filteredRes.push(res);
+                    includedTopPlayers.push(res.personId);
+                }
+            }
+            filteredRes.sort(function(a,b){return a['ranking'] - b['ranking']});
+            const finalLength = filteredRes.length;
+            
+            if (finalLength < 3 && num_rounds > 1) {
+                var filteredPrevRoundRes = [];
+                const previousRound = eventRounds[num_rounds-2];
+                for (var r in previousRound.results) {
+                    const res = previousRound.results[r];
+                    if (targetIds.includes(res.personId) && !includedTopPlayers.includes(res.personId)) {
+                        filteredPrevRoundRes.push(res);
+                    }
+                }
+                filteredPrevRoundRes.sort(function(a,b){return a['ranking'] - b['ranking']});
+                for (var p = 0; p < (3 - finalLength); p++) {
+                    filteredRes.push(filteredPrevRoundRes[p]);
+                }
+            }
+
             slide['title'] = [
                 compName,
                 eventNames[eventId],
-                "Open Category"
+                specialCategoryName,
             ];
-            const format = formats[lastRound.format]
+            slide['format'] = format.name;
+            slide['results'] = [];
+            const maxRank = Math.min(3, filteredRes.length);
+            for (var rank = maxRank; rank >= 1; rank--){
+                const res = filteredRes[rank - 1];
+                fillRow(res, rank, format, slide);
+            }
+            slides.push(slide);
+
+            var slide = {};
+            // open category
+            slide['title'] = [
+                compName,
+                eventNames[eventId],
+                openCategoryName,
+            ];
             slide['format'] = format.name;
             slide['results'] = [];
             for (var rank = 3; rank >= 0; rank--){
                 for (var r in lastRound.results) {
                     const res = lastRound.results[r];
                     if (res['ranking'] == rank) {
-                        const person = idToPerson[res.personId];
-                        const countryCode = person.countryIso2;
-                        const countryName = regionNames.of(countryCode);
-                        const countryFlag = getFlag(countryCode);
-                        const resTime = res[format.res];
-                        
-                        var resText = renderTime(resTime);
-                        if (eventId === '333mbf') {
-                            resText = renderMBTime(resTime);
-                        }
-                        slide['results'].push([
-                            rankToAward[rank],  countryFlag + ' ' + countryName,
-                            person.name, resText
-                        ]);
+                        fillRow(res, rank, format, slide);
                     }
                 }
             }
             slides.push(slide);
+        }
+
+        function fillRow(res, rank, format, slide) {
+            const person = idToPerson[res.personId];
+
+            const countryCode = person.countryIso2;
+            const countryName = regionNames.of(countryCode);
+            const countryFlag = getFlag(countryCode);
+            const countryText = countryFlag + ' ' + countryName;
+
+            const resTime = res[format.res];
+
+            var resText = renderTime(resTime);
+            if (eventId === '333mbf') {
+                resText = renderMBTime(resTime);
+            }
+
+            slide['results'].push([
+                rankToAward[rank], countryText, person.name, resText
+            ]);
         }
     });
     
