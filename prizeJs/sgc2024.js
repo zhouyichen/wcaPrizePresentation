48bf8f18bfdb750ed9621d0ba5e474bcc4685748
zhouyichen <zhouyichen123@gmail.com>
2024-01-11 21:03:44 +0800

    retrieve remote control signal

--- prizeJs/sgc2024.js
+++ prizeJs/sgc2024.js
@@ -13,3 +13,27 @@ console.log('compId:' + compId);
 const specialCategoryName = "Singaporean Category";
 let openCategoryName = "Open Category";
 
+$(function() {
+	var interval = 1000;
+	window.setInterval(function(){
+		$.ajax({
+			type: 'get',
+			url: 'https://gxqc1gfygd.execute-api.ap-southeast-1.amazonaws.com/production/slides/'+compId+'/get_prog_state',
+			data: {},
+			success: function(data) {
+				console.log(data);
+                var state = data.state;
+				if (state != 'pending') {
+					interval = 3000;
+					if (state == 'next') {
+						Reveal.next();
+					} else {
+						Reveal.prev();
+					}
+				} else {
+					interval = 1000;
+				}
+			}
+		});
+	}, interval);
+});
